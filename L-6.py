9d8969174572f64ef102ca12b407ca481e971d2c
kira_b <kirabogdana19@gmail.com>
2024-03-10 09:44:58 +0200

    l-6

--- L-6.py
+++ L-6.py
@@ -1,6 +1,9 @@
-import inspect
-import sys
-import requests
+from colorama import init, Fore, Back
 
-print(sys.executable)
-print(inspect.ismodule((requests)))
+init()
+
+print(Fore.RED + 'Цей текст червоний.')
+print(Fore.GREEN + 'Цей текст зелений.')
+print(Back.YELLOW + 'Цей текст має жовтий фон.')
+
+print(Fore.RESET + Back.RESET + 'Цей текст має стандартний колір тексту та фону.')
